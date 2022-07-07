f6bde5f1c80503dee54709065a03fdb10c677751
Atelu Victor Edem <52033662+Atelu@users.noreply.github.com>
2022-07-07 18:53:14 +0000

    verify account UI complete

--- src/account/password/reset-password.model.ts
+++ src/account/password/reset-password.model.ts
@@ -2,4 +2,6 @@ import { ResetPasswordInput } from '@shared/service-proxies/service-proxies';
 
 export class ResetPasswordModel extends ResetPasswordInput {
     public passwordRepeat: string;
+    public password: string;
+
 }
