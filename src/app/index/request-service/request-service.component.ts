d6705d2d4579df22effb440bf9f15a752bac551d
David <kofiahendev@gmail.com>
2022-07-14 08:57:28 +0000

    added upload images to service request

--- src/app/index/request-service/request-service.component.ts
+++ src/app/index/request-service/request-service.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-request-service',
-  templateUrl: './request-service.component.html',
-  styleUrls: ['./request-service.component.css']
+    selector: 'app-request-service',
+    templateUrl: './request-service.component.html',
+    styleUrls: ['./request-service.component.css'],
 })
 export class RequestServiceComponent implements OnInit {
+    step: number = 1;
+    showStepOne: boolean = false;
+    showStepTwo: boolean = true;
+    lightboxImages: any = [];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    showNext(): void {
+        this.showStepOne = !this.showStepOne;
+        this.showStepTwo = !this.showStepTwo;
+        this.step = 2;
+    }
+
+    addPicture(event: any) {
+        if (event.target.files && event.target.files[0]) {
+            for (const image of event.target.files) {
+                const reader = new FileReader();
+                reader.onload = (event: any) => {
+                    this.lightboxImages.push({ name: image.name, size: image.size, path: event.target.result });
+                    this.lightboxImages.reverse();
+                    event.target.value = '';
+                };
+
+                reader.readAsDataURL(image);
+            }
+        }
+    }
 }
