cdb7df5bc593b20dc81617a4fc3543bf6a90088f
David Sackey <davidkofiahensackey@gmail.com>
2022-08-16 01:24:47 +0000

    package fix

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(requireHTTPS);
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/abp-zero-template'));
 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/abp-zero-template/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
